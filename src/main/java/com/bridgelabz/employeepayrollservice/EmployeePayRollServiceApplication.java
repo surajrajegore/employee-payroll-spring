9e87b01d79bc68501933e12cec565598ccaee471
surajrajegore <rajegoresuraj2880@gmail.com>
2022-08-20 17:03:39 +0530

    Added:created payroll application

---
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayRollServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayRollServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePayRollServiceApplication.class, args);
+    }
+
+}
