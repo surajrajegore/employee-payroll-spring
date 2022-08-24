bc31843f436210596c716f7429c7fa2c4422f966
surajrajegore <rajegoresuraj2880@gmail.com>
2022-08-24 10:19:06 +0530

    Added:code of slf4j library

--- src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayRollServiceApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayRollServiceApplication.java
@@ -1,14 +1,20 @@
 package com.bridgelabz.employeepayrollservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(scanBasePackages =  {"com.bridgelabz.employeepayrollservice.service"})
+@Slf4j
 public class EmployeePayRollServiceApplication {
 
     public static void main(String[] args) {
         System.out.println("welcome to employee payroll");
-        SpringApplication.run(EmployeePayRollServiceApplication.class, args);
+        ApplicationContext context = SpringApplication.run(EmployeePayRollServiceApplication.class, args);
+
+        log.info("Employee payroll Started in envornmet",context.getEnvironment().getProperty("environment"));
+        log.info("Employee Payroll DB User name is ", context.getEnvironment().getProperty("spring.datasource.username"));
     }
 
 }
