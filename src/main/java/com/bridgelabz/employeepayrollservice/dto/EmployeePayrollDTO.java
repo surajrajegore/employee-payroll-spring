4c64063f46a1a85230727b3eb46b2012c9e272dc
surajrajegore <rajegoresuraj2880@gmail.com>
2022-08-26 06:58:52 +0530

    Added:code to validate salary and handle exception

--- src/main/java/com/bridgelabz/employeepayrollservice/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/dto/EmployeePayrollDTO.java
@@ -1,11 +1,13 @@
 package com.bridgelabz.employeepayrollservice.dto;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 
 public class EmployeePayrollDTO {
 
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\\\s]{2,}$", message = "name is invalid")
     public String name;
+    @Min(value = 500,message = "salary must be above 500")
     public  long salary;
 
     public EmployeePayrollDTO(String name, long salary) {
---
+++ src/main/java/com/bridgelabz/employeepayrollservice/exception/ExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.employeepayrollservice.exception;
+
+import com.bridgelabz.employeepayrollservice.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+        List<String> errMesg = errorList.stream().map(objErr -> objErr.getDefaultMessage())
+                .collect(Collectors.toList());
+        ResponseDto responseDTO = new ResponseDto("Exception while processing REST request", errMesg);
+        return new ResponseEntity<ResponseDto>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+}
